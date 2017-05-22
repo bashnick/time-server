039517d554a4057cd52f9e81cc01dc8ce0cb7683
Nikolay <bashnick@gmail.com>
2017-05-22 22:22:50 +0300

    Updated Current time and time in GMT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <h2>Current time is: {{currentTime}}</h2>
+    <h2>Time in GMT is: {{utcTime}}</h2>
+  `
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+  currentTime: string;
+  utcTime: string;
+  constructor() {
+    let d = new Date();
+    this.currentTime = d.toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, '$1');
+    this.utcTime = d.toUTCString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, '$1');
+  };
+}
